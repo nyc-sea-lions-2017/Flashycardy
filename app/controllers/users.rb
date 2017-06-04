f7f8e734e70383b0f869c166ebd090640216d41a
cfrancisco726 <cfrancisco726@gmail.com>
2017-06-04 18:42:09 -0400

    fix registration show

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,7 +6,7 @@ post '/users' do
   @user = User.new(params[:user])
   if @user.save
     session[:user_id] = @user.id
-    redirect '/'
+    redirect '/users/:id'
   else
     @errors = @user.errors.full_messages
     erb :"/users/new"
